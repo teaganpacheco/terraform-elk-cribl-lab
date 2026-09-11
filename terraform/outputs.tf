b301c3148f8a515d7a1922e1a618fd10b695cb86
teaganpacheco <114369061+teaganpacheco@users.noreply.github.com>
2026-09-11 15:40:28 -0500

    terraform-files-01

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,14 @@
-# asdf 
+output "aws_account_id" {
+  description = "AWS account in which Terraform is operating."
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "aws_region" {
+  description = "AWS region used by the lab."
+  value       = var.aws_region
+}
+
+output "availability_zone" {
+  description = "Availability Zone selected for the development lab."
+  value       = local.availability_zone
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,10 @@
-# asdf
+terraform {
+  required_version = "~> 1.16.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.62"
+    }
+  }
+}
